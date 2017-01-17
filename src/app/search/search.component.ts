d252b45de4d7550d28d5bc1216a79db6cac75401
dreiv <Solidchest1>
2017-01-18 00:14:27 +0200

    getting late :(

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,27 +1,65 @@
-import { Component, OnInit } from '@angular/core';
-import { SuggestService} from "./search.service";
-import { FormControl, AbstractControl } from "@angular/forms";
-import { Observable } from "rxjs";
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/distinctUntilChanged';
-import 'rxjs/add/operator/switchMap';
+import { Component, OnInit, OnDestroy, ElementRef } from "@angular/core";
+import { SuggestService } from "./search.service";
+import { FormControl } from "@angular/forms";
+import { Observable, Subscription } from "rxjs";
+import "rxjs/add/operator/debounceTime";
+import "rxjs/add/operator/distinctUntilChanged";
+import "rxjs/add/operator/switchMap";
+
+enum Key {
+  ArrowUp = 38,
+  ArrowDown = 40
+}
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
-  query: AbstractControl = new FormControl();
+export class SearchComponent implements OnInit, OnDestroy {
+  query: FormControl = new FormControl();
   suggestions: Observable<Array<string>>;
+  suggestionIndex: number;
 
-  constructor(private suggestService: SuggestService) {}
+  subscriptions: Subscription[];
+  element: ElementRef;
+
+  constructor(element: ElementRef, private suggestService: SuggestService) {
+    this.element = element;
+  }
 
   ngOnInit(){
     this.suggestions = this.query.valueChanges
       .debounceTime(400)
       .distinctUntilChanged()
       .switchMap(query => this.suggestService.suggest(query));
+
+    this.subscriptions = [
+        this.navigateWithArrows()
+    ];
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+    this.subscriptions.length = 0;
+  }
+
+  navigateWithArrows() {
+    return Observable.fromEvent(this.element.nativeElement, 'keydown')
+      .filter((e: any) => e.keyCode === Key.ArrowDown || e.keyCode === Key.ArrowUp)
+      .map((e: any) => e.keyCode)
+      .subscribe((keyCode: number) => {
+        let step = keyCode === Key.ArrowDown ? 1 : -1;
+        const topLimit = this.suggestions.toArray.length;
+        const bottomLimit = 0;
+        this.suggestionIndex += step;
+        if (this.suggestionIndex === topLimit + 1) {
+          this.suggestionIndex = bottomLimit;
+        }
+        if (this.suggestionIndex === bottomLimit - 1) {
+          this.suggestionIndex = topLimit;
+        }
+      });
   }
 
 }

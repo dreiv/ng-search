e8c7cfecb63d13ac3609a3e4c1eee9dc597d9059
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2017-01-18 11:28:49 +0200

    Zone js was fixed overnight, so there is no need for unnecesary imports any more.

--- src/app/search/search.service.ts
+++ src/app/search/search.service.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@angular/core';
-import { URLSearchParams, Jsonp } from "@angular/http";
-import 'rxjs/add/operator/map';
+import { URLSearchParams, Jsonp } from '@angular/http';
 
 @Injectable()
 export class SuggestService {
 
-  constructor(private jsonp: Jsonp) {}
+    constructor(private jsonp: Jsonp) {
+    }
 
-  suggest(query: string) {
-    var search = new URLSearchParams()
-    search.set('action', 'opensearch');
-    search.set('search', query);
-    search.set('format', 'json');
-    return this.jsonp
-      .get('http://en.wikipedia.org/w/api.php?callback=JSONP_CALLBACK', { search })
-      .map((response) => response.json()[1]);
-  }
+    suggest(query: string) {
+        var search = new URLSearchParams()
+        search.set('action', 'opensearch');
+        search.set('search', query);
+        search.set('format', 'json');
+        return this.jsonp
+            .get('http://en.wikipedia.org/w/api.php?callback=JSONP_CALLBACK', {search})
+            .map((response) => response.json()[1]);
+    }
 }
